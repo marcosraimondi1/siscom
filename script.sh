afbf4a82e6fa077094fd0be7c2e865703f9bfe8e
marcosraimondi <marcosraimondi1@hotmail.com>
2024-03-22 19:52:13 -0300

    comandos perf

--- script.sh
+++ script.sh
@@ -15,3 +15,12 @@ gprof ./test_gprof gmon.out > profile.txt
 
 # create image
 cat profile.txt | gprof2dot | dot -Tpng -o profile.png
+
+# compile with debugging information
+gcc -Wall -g test_gprof.c test_gprof_new.c -o ./build/test_gprof
+
+# run perf profiler
+perf record -g -- /path/to/your/executable
+
+# create image
+perf script | c++filt | gprof2dot.py -f perf | dot -Tpng -o output.png
